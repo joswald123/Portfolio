cb0a4e13ef927992c00bab8d29f0b0780f0ca07c
joswald123 <julianaandreaoswald@gmail.com>
2022-07-06 16:46:06 -0700

    added pics, update data, created handle error funtions...

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
+//Routes
 var indexRouter = require('./routes/index');
-
+app.use('/', indexRouter);
 
 //view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,10 +13,26 @@ app.set('view engine', 'pug');
 //static middleware
 app.use(express.static('public'));
 
-//Routes
-app.use('/', indexRouter);
+
+// Error Handlers Response 404
+app.use((req, res, next) => {
+    res.status(404).render('not-found')
+    
+});
+
+// Global error Handler Response 404 or 500
+app.use((err, req, res, next) => {
+
+    if(err.status === 404) {
+        res.status(404).render('not-found', { err });
+    } else {
+        err.message = err.message || 'It looks like something went wrong on the server.';
+        res.status(err.status || 500).render('error', { err })
+    }        
+  });
 
 
+// Listening port
 app.listen(3000, () => {
     console.log("The application is running on localhost:3000!")
 });
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { projects } = require('../data.json');
+const { projects } = require("../data.json");
 
 /* GET home page */
-router.get('/', function(req, res, next) {
-    res.render('index', { projects })
+router.get("/", function (req, res, next) {
+  res.render("index", { projects });
 });
 
 /* GET about page */
-router.get('/about', function(req, res, next) {
-    res.render('about', { projects })
+router.get("/about", function (req, res, next) {
+  res.render("about", { projects });
 });
 
 /* GET project page */
-router.get('/projects/:id', function(req, res, next) {
-    const projectId = req.params.id;
-    const project = projects.find( ({ id }) => id === +projectId );
+router.get("/projects/:id", function (req, res, next) {
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
 
-    if(project) {
-        res.render('project', { projects });
-    } else {
-        console.log("Error 404");
-    }
-    
-
+  if (project) {
+    res.render("project", { project });
+  } else {
+    console.log("Error 404");
+    res.status(404).render("not-found");
+  }
 });
 
+/* GET generated error route - create and throw 500 server error */
+router.get("/error", (req, res, next) => {
+  const err = new Error();
+  err.message = `Custom 500 error thrown`;
+  err.status = 500;
+  throw err;
+});
 
 module.exports = router;
